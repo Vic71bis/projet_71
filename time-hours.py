737601e23de69519590b4dc6e01056fdf7f2697d
Ilona0705 <166836455+Ilona0705@users.noreply.github.com>
2024-05-17 17:23:20 +0200

    Update time-hours.py

--- time-hours.py
+++ time-hours.py
@@ -5,23 +5,22 @@ Created on Fri May 17 17:04:17 2024
 @author: ifyes
 """
 
-def convert_time (hours, minutes):
-    """
-    Parameters
-    ----------
-    hours : string
-
-    minutes : string
-
-
-    Returns
-    -------
-    time in hours
-
-    """
+def convert_time_hours (hours, minutes):
+    
     h = int(hours)
     m = int(minutes)/60
     tot_time = h + m
     
-    return tot_time
+    return tot_time  # tot time in hours
+
+
+
+def convert_time_h_min (hours):
+    
+    entire_hours = int(hours)
+    min_in_hours = entire_hours - hours
+    minutes = min_in_hours/60
+    
+    return entire_hours, minutes 
+    
     
